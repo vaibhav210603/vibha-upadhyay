a2b38b8ba1aac1a7c44e7e189f5a7263f9f3a369
vaibhav <vaibhav210603@gmail.com>
2025-05-15 11:57:14 +0530

    fixed reload issues once again

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
+    sourcemap: true
   }
 })
